300f0ffac0626747f3242da2d17b9a6fe746bebb
mlevans <mikevanswork@gmail.com>
2010-09-28 02:26:54 -0700

    first commit

--- SF Crime Data/SFCrimeKMLToJSON.java
+++ SF Crime Data/SFCrimeKMLToJSON.java
@@ -1,10 +1,8 @@
-//In Progress!
-
 package sfcrimekmltojson;
 
 /**
  *
- * @author Owner
+ * @author Michael Lawrence Evans :: michael@longliveman.com
  */
 import java.io.*;
 
@@ -12,6 +10,13 @@ public class Main {
     
      BufferedReader reader = null;
      BufferedWriter writer = null;
+
+     int count = 0;
+
+     String Categories[] = new String[3000];
+     String Dates[] = new String[3000];
+     String Lons[] = new String[3000];
+     String Lats[] = new String[3000];
 
     public static void main(String[] args) {
         Main sfKMLToJSON = new Main();
@@ -43,8 +48,7 @@ public class Main {
        }
 
        while(s.compareTo("<Placemark") == 0){
-           //s = getNonEmptyString(); //getting the string after placemark
-           //while(s.compareTo("\"Category\">")) //escaping quotes with backslash
+
            while (true) { //always do it
                 int placemarkTopicStringIndex = 0;
                 int placemarkTopicStringStartIndex;
@@ -58,11 +62,11 @@ public class Main {
                     placemarkTopicStringStartIndex = placemarkTopicStringIndex + 11;
 
                     placemarkTopicEndIndex = placemarkLine.indexOf("<", placemarkTopicStringStartIndex); //Starting from
-                    //placemarkTopicStringIndex+10 find the next "<"
+                    //placemarkTopicStringIndex+11; find the next "<"
 
                     String category = placemarkLine.substring(placemarkTopicStringStartIndex, placemarkTopicEndIndex);
 
-                    //System.out.println(category);
+                    Categories[count] = category;
 
                 }
 
@@ -72,22 +76,22 @@ public class Main {
                     placemarkTopicStringStartIndex = placemarkTopicStringIndex + 7;
 
                     placemarkTopicEndIndex = placemarkLine.indexOf("<", placemarkTopicStringStartIndex); //Starting from
-                    //placemarkTopicStringIndex+6 find the next "<"
+                    //placemarkTopicStringIndex+7; find the next "<"
 
                     String date = placemarkLine.substring(placemarkTopicStringStartIndex, placemarkTopicEndIndex);
 
-                    //System.out.println(date);
+                    Dates[count] = date;
                 }
 
                 placemarkTopicStringIndex = placemarkLine.indexOf("coordinate"); //gives you the position of the first letter of the
-                //first occurence of coordinate
+                //first occurrence of coordinate
 
                 if (placemarkTopicStringIndex >= 0) {
-                    //Longitude
+                    //Handle the Longitudes
                     placemarkTopicStringStartIndex = placemarkTopicStringIndex + 12;
 
                     placemarkTopicEndIndex = placemarkLine.indexOf(".", placemarkTopicStringStartIndex); //Starting from
-                    //placemarkTopicStringIndex+12 find the next "."
+                    //placemarkTopicStringIndex+12; find the next "."
 
                     String startLon = placemarkLine.substring(placemarkTopicStringStartIndex, placemarkTopicEndIndex);
 
@@ -100,13 +104,12 @@ public class Main {
 
                     String Lon = startLon + endLon; //concatenate the string
 
-                    //System.out.println(Lon);
+                    Lons[count] = Lon;
 
-                    //Latitude
+                    //Handle the Latitudes
                     placemarkTopicStringStartIndex = placemarkLine.indexOf(",", placemarkTopicStringStartIndex) + 1;
 
-                    placemarkTopicEndIndex = placemarkLine.indexOf(".", placemarkTopicStringStartIndex); //Starting from
-                    //placemarkTopicStringIndex+12 find the next "."
+                    placemarkTopicEndIndex = placemarkLine.indexOf(".", placemarkTopicStringStartIndex);
 
                     String startLat = placemarkLine.substring(placemarkTopicStringStartIndex, placemarkTopicEndIndex);
 
@@ -119,19 +122,28 @@ public class Main {
 
                     String Lat = startLat + endLat; //concatenate the string
 
-                    System.out.println(Lat);
+                    Lats[count] = Lat;
 
                     break;
                 }
             }
 
+            count++;
+            
              s = getNonEmptyString();
 
              while(s.compareTo("</Placemark>") != 0){
-                s = getNonEmptyString(); //Reading through the data until we get to our data blocks
+                s = getNonEmptyString();
             }
 
              s = getNonEmptyString();
+       }
+
+       try{
+           outputFile();
+           writer.close();
+       } catch(IOException e) {
+           System.exit(1);
        }
     }
 
@@ -173,7 +185,7 @@ public class Main {
         s = strings[0]; //Getting the first string in the strings array
 
         if (strings.length == 1){
-            //Rarely Happens, but there was only one string on the line
+            //Rarely Happens, but this occurs when there is only one string on the line
             stringIndex = -1;
         } else {
             stringIndex = 1;
@@ -204,5 +216,61 @@ public class Main {
          }
 
         return result;
+     }
+
+     public void outputFile() throws IOException{
+
+	 //Use the data to create a file in JSON format
+
+         int i;
+         
+         System.out.println(count);
+         writer.write("{", 0, 1);
+         writer.newLine();
+         writer.write(" " + "\"incidents\":{", 0, 14);
+         writer.newLine();
+         writer.write(" " + "\t" + "\"incident\":[", 0, 14);
+         writer.newLine();
+
+         for (i=0; i < count; i++){
+
+             writer.write(" " + "\t" + "{",0,3);
+             writer.newLine();
+             writer.write(" " + "\t" + "\"category\":" + "\"" + Categories[i] + "\",", 0, Categories[i].length() + 16);
+             writer.newLine();
+             writer.write(" " + "\t" + "\"date\":" + "\"" + Dates[i] + "\",", 0, Dates[i].length() + 12);
+             writer.newLine();
+             writer.write(" " + "\t" + "\"long\":" + "\"" + Lons[i] + "\",", 0, Lons[i].length() + 12);
+             writer.newLine();
+             writer.write(" " + "\t" + "\"lat\":" + "\"" + Lats[i] + "\"", 0, Lats[i].length() + 10);
+             writer.newLine();
+
+             if (i < count - 1){
+                writer.write(" " + "\t" + "},",0,4);
+             } else {
+                 writer.write(" " + "\t" + "}",0,3);
+             }
+             writer.newLine();
+         }
+             /* Using an if test above, so we don't need to use so much code
+             writer.write(" " + "\t" + "{",0,3);
+             writer.newLine();
+             writer.write(" " + "\t" + "\"category\":" + "\"" + Categories[count-1] + "\"", 0, Categories[count-1].length() + 15);
+             writer.newLine();
+             writer.write(" " + "\t" + "\"date\":" + "\"" + Dates[count-1] + "\"", 0, Dates[count-1].length() + 11);
+             writer.newLine();
+             writer.write(" " + "\t" + "\"long\":" + "\"" + Lons[count-1] + "\"", 0, Lons[count-1].length() + 11);
+             writer.newLine();
+             writer.write(" " + "\t" + "\"lat\":" + "\"" + Lats[count-1] + "\"", 0, Lats[count-1].length() + 10);
+             writer.newLine();
+             writer.write(" " + "\t" + "}",0,3);
+             writer.newLine();
+             */
+             writer.write("\t" + "]",0,2);
+             writer.newLine();
+             writer.write(" " + "}",0,2);
+             writer.newLine();
+             writer.write("}",0,1);
+
      }
 }
